11d76b4068a3cfc87a098ad9d98ebb7f203cc1de
George Benainous <georgesb@mac.com>
2020-01-21 23:54:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 void setup() {
-  size(640, 360);
-  background(220);
+	size(600, 600);
+  background(255);
+  drawGrid();
 }
 
+// Write your code inside the draw() function
 void draw() {
 
+
 }
+
+
+
+void drawGrid() {
+
+    strokeWeight(0.1);
+		fill(0);
+      for (int x = -width; x < width; x += 50) {
+        line(x, -height, x, height);
+        text(x, x + 1, 12);
+      }
+      for (int y = -height; y < height; y += 50) {
+        line(-width, y, width, y);
+        text(y, 1, y + 12);
+      }
+  }
+
+
+
+
 
